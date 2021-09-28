5e7fe6c8bcf5d8f7aa9468238b90cd07ad1fbe8f
teohangxanh <dtnghia1987@gmail.com>
2021-09-28 15:31:31 -0700

    Successfully created order class

--- Project 2/Immplementation.py
+++ Project 2/Immplementation.py
@@ -2,16 +2,23 @@ import random
 from faker import Faker
 
 
-def create_order():
+class Order:
     '''
     This creates an order with random numbers representing
     divivion, items ordered, shelf
     Assumption: The quantity of each item is 1, and the number of shelves is equal to the number of items
     '''
-    fake_data = Faker()
     division = random.randint(1, 15)
     number_of_items = random.randint(1, 3)
     shelves = random.sample(range(1, 63), number_of_items)
-    return (division, [{shelf: fake_data.ean(length=13)} for shelf in shelves])
 
-print(create_order())
+    def __init__(self):
+        self.divison = Order.division
+        self.shelves = [{shelf: Faker().ean(length=13)} for shelf in Order.shelves]
+
+    def __repr__(self):
+        return f'Divison: {self.division}\nShelf & barcode: {self.shelves}'
+
+
+order = Order()
+print(order)
