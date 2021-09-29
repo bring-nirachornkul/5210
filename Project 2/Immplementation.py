f5d9ea67687135c1ca9ef0b78e92475efcac4d5f
teohangxanh <dtnghia1987@gmail.com>
2021-09-29 00:05:56 -0600

    Project 2 added a print tree method

--- Project 2/Immplementation.py
+++ Project 2/Immplementation.py
@@ -20,21 +20,35 @@ class Order:
         return f'Divison: {self.division}\nShelf & barcode: {self.shelves}'
 
 
-# Source: https://stackoverflow.com/questions/2598437/how-to-implement-a-binary-tree
 class Node:
     def __init__(self, weight=0, number=1):
         self.l = None
         self.r = None
         self.weight = weight
         self.number = number
+
     def __repr__(self):
         return f'Node number {self.number} has weight of {self.weight}'
 
+
 class Tree:
     count = 0
+    weights = []
+
+    def print_tree(self):
+        '''
+        This method prints a list of all nodes and their respective weights
+        '''
+        if Tree.count > 0:
+            print([(count, weight) for count, weight in zip(range(1, Tree.count + 1), Tree.weights)])
+        else:
+            print('The tree needs to have some nodes')
 
     @staticmethod
     def halves(n):
+        '''
+        This method returns the path from the root node to the current one
+        '''
         r = []
         path = ''
         while n != 0:
@@ -46,6 +60,7 @@ class Tree:
         self.root = Node()
         Tree.count += 1
         self.root.number = Tree.count
+        Tree.weights.append(0)
 
     def getRoot(self):
         return self.root
@@ -53,6 +68,9 @@ class Tree:
     def add(self, weight):
         if self.root is None:
             self.root = Node()
+            Tree.count += 1
+            self.root.number = Tree.count
+            Tree.weights.append(0)
         else:
             current = self.getRoot()
             Tree.count += 1
@@ -63,18 +81,18 @@ class Tree:
             else:
                 paths = self.halves(Tree.count)[:-1]
                 cur = paths[0]
-                print(f'paths: {paths}, cur: {cur}')
                 for path in paths[1:]:
                     if cur * 2 == path:
                         current = current.l
                     else:
                         current = current.r
                     cur = path
-                print(current.weight, current.number)
                 if cur * 2 == Tree.count:
                     current.l = Node(weight, Tree.count)
                 else:
                     current.r = Node(weight, Tree.count)
+        Tree.weights.append(weight)
+
 
 tree = Tree()
 tree.add(20)
@@ -83,4 +101,4 @@ tree.add(20)
 tree.add(30)
 tree.add(40)
 tree.add(10)
-
+tree.print_tree()
