2811872febf54df40428be36dfc35ca76e4a562a
teohangxanh <dtnghia1987@gmail.com>
2021-10-02 08:55:05 -0600

    Project 2 method add and ids were corrected

--- Project 2/Immplementation.py
+++ Project 2/Immplementation.py
@@ -87,37 +87,39 @@ class Tree:
             self.root = Node()
             self.tail = self.root
         else:
-            current = self.getRoot()
-            next_node = current
+            current_node = self.getRoot()
+            next_node = current_node
             if self.tail.number == 1:
-                current.l = Node(cost)
-                next_node = current.l
-                next_node.parent = current
+                current_node.l = Node(cost)
+                next_node = current_node.l
+                next_node.parent = current_node
             elif self.tail.number == 2:
-                current.r = Node(cost)
-                next_node = current.r
-                next_node.parent = current
+                current_node.r = Node(cost)
+                next_node = current_node.r
+                next_node.parent = current_node
             else:
                 paths = self.halves(self.tail.number + 1)[:-1]
-                cur = paths[0]
+                # print(f'tail = {self.tail.number}, paths = {paths}')
                 for path in paths[1:]:
-                    if cur * 2 == path:
-                        current = current.l
+                    if current_node.number * 2 == path:
+                        current_node = current_node.l
                     else:
-                        current = current.r
-                    cur = path
-                if cur * 2 == self.tail.number:
-                    current.l = Node(cost, self.tail.number)
-                    next_node = current.l
-                    next_node.parent = current
+                        current_node = current_node.r
+                # print(f'After for loop, current node = {current_node.number}')
+                if current_node.number * 2 == self.tail.number + 1:
+                    current_node.l = Node(cost, self.tail.number + 1)
+                    next_node = current_node.l
+                    next_node.parent = current_node
+                    # print(f'Added {next_node.number} to the left of {current_node.number}')
                 else:
-                    current.r = Node(cost, self.tail.number)
-                    next_node = current.r
-                    next_node.parent = current
+                    current_node.r = Node(cost, self.tail.number + 1)
+                    next_node = current_node.r
+                    next_node.parent = current_node
+                    # print(f'Added {next_node.number} to the right of {current_node.number}')
+                print()
             next_node.level = self.find_level(self.tail.number)
             next_node.number = self.tail.number + 1
             self.tail = next_node
-
 
     def find_node(self, number):
         '''Returns a path from root to the node which has the number
@@ -170,19 +172,16 @@ class Tree:
                 print(f'parent is {root.parent.number}')
             print()
 
-    def ids(self, number, depth_limit=None):
+    def ids(self, number):
         '''
         This method makes use of iterative deepening search for a specific node
+        Assumption: The depth limit can be the tree's depth
         '''
-        if number < 1 or number > Tree.count:
+        if number < self.root.number or number > self.tail.number:
             return f'The node number does not exist in the tree'
-        if depth_limit == None:
-            depth_limit = Tree.depth
         visited = []
         current_node = self.getRoot()
-        while current_node and current_node.level <= depth_limit - 1 and current_node.number != number:
-            print(
-                f'Current node number: {current_node.number},  level: {current_node.level}, depth limit: {depth_limit}')
+        while current_node and current_node.number != number:
             if current_node.number not in visited:
                 visited.append(current_node.number)
             if current_node.l and current_node.l.number not in visited:
@@ -191,7 +190,7 @@ class Tree:
                 current_node = current_node.r
             else:
                 current_node = current_node.parent
-        print(f'Current node after while loop: {current_node.number}')
+        # print(f'Current node after while loop: {current_node.number}')
         if not current_node or current_node.number != number:
             return f'The node number {number} could not be found'
         else:
@@ -200,17 +199,16 @@ class Tree:
 
 
 costs = [20, 20, 20, 30, 40, 10, 10, 20, 30, 20, 30, 20, 20, 20]
-divisons = Tree('divisions')
+divisions = Tree('divisions')
 for cost in costs:
-    divisons.add(cost)
-# divisons.print_tree()
-
-shelves = Tree('shelves')
-for i in range(63):
-    shelves.add(1)
+    divisions.add(cost)
+# divisions.print_tree()
+# shelves = Tree('shelves')
+# for i in range(63):
+#     shelves.add(1)
 # shelves.print_tree()
 
-# tree.ids(15)
+print(divisions.ids(15))
 # print(tree.back_track(12))
 # print(tree.find_node(12))
 # root = tree.getRoot()
